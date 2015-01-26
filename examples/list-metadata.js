c5848908d13b5b305691a023c49207fdd48d2938
Kevin O'Hara <kevinohara80@gmail.com>
2015-01-25 22:33:12 -0500

    listMetadata functional

---
+++ examples/list-metadata.js
@@ -0,0 +1,30 @@
+var nforce = require('nforce');
+var _      = require('lodash');
+
+require('../')(nforce);
+
+var org = nforce.createConnection({
+  clientId: '3MVG9rFJvQRVOvk5nd6A4swCyck.4BFLnjFuASqNZmmxzpQSFWSTe6lWQxtF3L5soyVLfjV3yBKkjcePAsPzi',
+  clientSecret: '9154137956044345875',
+  redirectUri: 'http://localhost:3000/oauth/_callback',
+  mode: 'single',
+  username: process.env.SFUSER,
+  password: process.env.SFPASS,
+  plugins: ['meta']
+});
+
+org.authenticate().then(function(){
+  return org.meta.listMetadata({
+    queries: [
+      { type: 'CustomObject' },
+      { type: 'ApexClass' },
+      { type: 'ApexPage' }
+    ]
+  });
+}).then(function(meta) {
+  _.each(meta, function(r) {
+    console.log(r.type + ': ' + r.fullName + ' (' + r.fileName + ')');
+  });
+}).error(function(err) {
+  console.error(err);
+});
--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-var _ = require('lodash');
+var soap = require('./lib/soap-client');
 
 module.exports = function(nforce, name) {
   // throws if the plugin already exists
   var plugin = nforce.plugin(name || 'meta');
 
+  // create local vars for some utils provided by nforce
+  var _              = nforce.util._;
+  var createResolver = nforce.util.promises.createResolver;
+
+  /* describe api call */
+
+  plugin.fn('describeApi', function(data, cb) {
+    var opts     = this._getOpts(data, cb);
+    var resolver = createResolver(opts.callback);
+
+    this.meta._getSoapClient(data, function(err, client) {
+      if(err) return resolver.reject(err);
+      return resolver.resolve(client.describe());
+    });
+
+    return resolver.promise;
+  });
+
   /* deploy api calls */
-  
+
   plugin.fn('deploy', function(data, cb) {
     var opts = this._getOpts(data);
   });
@@ -77,13 +95,52 @@ module.exports = function(nforce, name) {
   });
 
   plugin.fn('listMetadata', function(data, cb) {
-    var opts = this._getOpts(data);
+    var opts = this._getOpts(data, cb);
+
+    opts.data = {
+      queries: opts.queries,
+      asOfVersion: opts.asOfVersion
+    };
+
+    opts.method = 'listMetadata';
+
+    return this.meta._apiRequest(opts, opts.callback);
   });
 
   /* low-level api calls */
 
-  plugin.fn('_metadataApiCall', function(data, cb) {
+  plugin.fn('_getSoapClient', function(data, cb) {
+    var self     = this;
+    var opts     = this._getOpts(data, cb);
+    var resolver = createResolver(opts.callback);
 
+    if(this.soapClient) {
+      resolver.resolve(this.soapClient);
+    } else {
+      soap.createClient(opts.oauth, function(err, client) {
+        if(err) return resolver.reject(err);
+        self.soapClient = client;
+        resolver.resolve(client);
+      });
+    }
+
+    return resolver.promise;
+  });
+
+  plugin.fn('_apiRequest', function(data, cb) {
+    var opts = this._getOpts(data);
+    var resolver = createResolver(opts.callback);
+
+    this.meta._getSoapClient(opts).then(function(client) {
+      client.MetadataService.Metadata[opts.method](opts.data, function(err, res) {
+        if(err) return resolver.reject(err);
+        else return resolver.resolve(res.result);
+      });
+    }).error(function(err) {
+      resolver.reject(err);
+    });
+
+    return resolver.promise;
   });
 
 };
